d1984a0e656adc1a67e4418cbe679e05abdb9cb4
I_Am_Stephan <stephanburger54@gmail.com>
2020-01-06 10:55:42 +0200

    contact form test

--- api/contact.js
+++ api/contact.js
@@ -1,17 +1,39 @@
-const sgMail = require('@sendgrid/mail');
+const sgMail = require('@sendgrid/mail')
 
-exports.handler = function(event, context, callback) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+function ID () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
 
-  const msg = {
-    to: 'stephan@avvent.io',
-    cc: ['armin@avvent.io'],
-    from: 'contact@gerimed.co.za',
-    subject: 'Test: send from node',
-    text: 'asdasd',
-    html: 'asdasd',
-  };
-  sgMail.send(msg);
+exports.handler =  async (event, context, callback) => {
 
-  console.log('sent')
+    const payload = JSON.parse(event.body)
+    const { customData } = payload
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+    const msg = {
+        to: 'stephan@avvent.io',
+        from: 'contact_form@gerimed.co,za',
+        subject: 'Contact Form Submission',
+        html: `
+            <h1> ${customData.name} : ${ID()} </h1>
+            <h2> ${customData.email} </h2>
+            <hr />
+            <p> ${customData.message} </p>
+        `,
+    };
+
+    try{
+        await sgMail.send(msg)
+        
+        return {
+            statusCode: 200,
+            body: 'Message sent'
+        }
+    } catch(e){
+        return {
+            statusCode: e.code,
+            body: e.message
+        }
+    }
 };
--- src/components/contactModal/contactModal.js
+++ src/components/contactModal/contactModal.js
@@ -2,9 +2,233 @@ import React, { Component } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Button } from '@material-ui/core';
 
 export default class ContactModal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: {
+        value: '',
+        error: false,
+        helperText: null
+      },
+      email: {
+        value: '',
+        error: false,
+        helperText: null
+      },
+      message: {
+        value: '',
+        error: false,
+        helperText: null
+      },
+      formWorking: false
+    }
+
+    this.setStateAsync = this.setStateAsync.bind(this)
+    this.onSubmitForm = this.onSubmitForm.bind(this)
+    this.resetForm = this.resetForm.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+    this.validateEmail = this.validateEmail.bind(this)
+  }
+
+  setStateAsync(state) {
+    return new Promise((resolve) => {
+      this.setState(state, resolve)
+    });
+  }
+
+  async onSubmitForm() {
+    let errors = false
+
+    await this.setStateAsync({
+      ...this.state,
+      formWorking: true
+    })
+
+    // Check For Form Errors
+    if(!this.state.name.value || this.state.name.value.length === 0 || /^\s*$/.test(this.state.name.value)) {
+      errors = true
+
+      await this.setStateAsync({
+        ...this.state,
+        name: {
+          ...this.state.name,
+          error: true,
+          helperText: 'Name cannot be empty'
+        }
+      })
+    } else {
+      await this.setStateAsync({
+        ...this.state,
+        name: {
+          ...this.state.name,
+          error: false,
+          helperText: null
+        }
+      })
+    }
+
+    if(!this.validateEmail(this.state.email.value)) {
+      errors = true
+      
+      await this.setStateAsync({
+        ...this.state,
+        email: {
+          ...this.state.email,
+          error: true,
+          helperText: 'Invalid email'
+        }
+      })
+    } else {
+      await this.setStateAsync({
+        ...this.state,
+        email: {
+          ...this.state.email,
+          error: false,
+          helperText: null
+        }
+      })
+    }
+
+    if(!this.state.message.value || this.state.message.value.length === 0 || /^\s*$/.test(this.state.message.value)) {
+      errors = true
+      
+      await this.setStateAsync({
+        ...this.state,
+        message: {
+          ...this.state.message,
+          error: true,
+          helperText: 'Message cannot be empty'
+        }
+      })
+    } else {
+      await this.setStateAsync({
+        ...this.state,
+        message: {
+          ...this.state.message,
+          error: false,
+          helperText: null
+        }
+      })
+    }
+
+    //Check if there was any errors
+    await this.setStateAsync({
+      ...this.state,
+      formWorking: false
+    })
+
+    if(!errors) {
+      let PostRequest = {
+        customData: {
+          name: this.state.name.value,
+          email: this.state.email.value,
+          message: this.state.message.value
+        }
+      }
+
+      try{
+        const response = await fetch('https://gerimed.netlify.com/.netlify/functions/contact', {
+          method: 'POST',
+          body: JSON.stringify(PostRequest),
+        })
+      
+        if (!response.ok) {
+          alert('Error: Your form has not been submited')
+          return
+        }
+
+        this.props.onClose()
+          
+      } catch(e){
+        await this.setStateAsync({
+          ...this.state,
+          formWorking: false
+        })
+        alert('Error: Your form has not been submited')
+      }
+
+      this.props.onClose()
+    }
+  }
+
+  resetForm() {
+    this.setState({
+      name: {
+        value: '',
+        error: false,
+        helperText: null
+      },
+      email: {
+        value: '',
+        error: false,
+        helperText: null
+      },
+      message: {
+        value: '',
+        error: false,
+        helperText: null
+      },
+      formWorking: false
+    })
+  }
+
+  handleChange(type, value) {
+    switch (type) {
+      case 'name':
+        this.setState({
+          ...this.state,
+          name: {
+            ...this.state.name,
+            value: value
+          }
+        })
+        break;
+
+      case 'email':
+        this.setState({
+          ...this.state,
+          email: {
+            ...this.state.name,
+            value: value
+          }
+        })
+        break;
+
+      case 'message':
+        this.setState({
+          ...this.state,
+          message: {
+            ...this.state.name,
+            value: value
+          }
+        })
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  closeModal() {
+    if(this.state.formWorking) {
+      return
+    } else {
+      this.resetForm()
+      this.props.onClose()
+    }
+  }
+
+  validateEmail(email) {
+    let regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+
+    return regexEmail.test(email)
+  }
+
   render() {
     return (
-      <Dialog open={this.props.open} onClose={this.props.onClose}>
+      <Dialog open={this.props.open} onClose={this.closeModal}>
         <DialogTitle>Send us a message</DialogTitle>
         <DialogContent>
           <DialogContentText>
@@ -13,35 +237,53 @@ export default class ContactModal extends Component {
 
           <TextField
             autoFocus
-            margin="dense"
-            id="name"
-            label="Full name"
+            error={this.state.name.error}
+            helperText={this.state.name.helperText}
+            margin='dense'
+            id='name'
+            label='Full name'
             type='text'
             fullWidth
+            onChange={(e) => this.handleChange('name', e.target.value)}
           />
           <TextField
-            margin="dense"
-            id="email"
-            label="Email"
+            error={this.state.email.error}
+            helperText={this.state.email.helperText}
+            margin='dense'
+            id='email'
+            label='Email'
             type='email'
             fullWidth
+            onChange={(e) => this.handleChange('email', e.target.value)}
           />
           <TextField
-            margin="dense"
-            id="message"
-            label="Message"
+            error={this.state.message.error}
+            helperText={this.state.message.helperText}
+            margin='dense'
+            id='message'
+            label='Message'
             type='text'
             multiline
             rows='5'
             fullWidth
+            onChange={(e) => this.handleChange('message', e.target.value)}
           />
         </DialogContent>
 
         <DialogActions>
-          <Button onClick={this.props.onClose} color="primary">
+          <Button
+            disabled={this.state.formWorking}
+            onClick={this.props.onClose}
+            color='primary'
+          >
             Cancel
           </Button>
-          <Button onClick={this.props.onClose} color="primary" variant='contained'>
+          <Button
+            disabled={this.state.formWorking}
+            onClick={this.onSubmitForm}
+            color='primary'
+            variant='contained'
+          >
             Send
           </Button>
         </DialogActions>
